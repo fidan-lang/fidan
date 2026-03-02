a2a4c47c0db954282dd5223dd751398f5864a508
AppSolves <appsolves@gmail.com>
2026-03-03 00:03:29 +0100

    refactor(test.bat): enhance output formatting for test execution logs

--- scripts/test.bat
+++ scripts/test.bat
@@ -6,10 +6,14 @@ cargo test -q 2>&1
 
 for /R ".\test" %%F in (*.fdn) do (
     if /I "%%~nxF"=="trace_demo.fdn" (
-        echo Running %%F with trace
+        echo(
+        echo === Running %%F with full trace ===
+        echo(
         .\target\debug\fidan run "%%F" --trace full
     ) else (
-        echo Running %%F
+        echo(
+        echo === Running %%F ===
+        echo(
         .\target\debug\fidan run "%%F"
     )
 )
